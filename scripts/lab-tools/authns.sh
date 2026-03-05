d8696bd542fe04d4a8453f902333e923cb2314ee
Ulrich Wisser <ulrich.wisser@icann.org>
2026-03-05 15:29:47 +0100

    fix authns configuration

--- scripts/lab-tools/authns.sh
+++ scripts/lab-tools/authns.sh
@@ -12,31 +12,27 @@ create_authns () {
     lxc exec authns -- cloud-init status --wait
     lxc config device add authns eth0 nic name=eth0 nictype=bridged parent=net-bb
 
-    # generating network config
-    sed -e "s|%IPv6pfx%|$IPv6prefix|g" 
+    # configure network
+    sed -e "s|%IPv6pfx%|$IPv6prefix|g" \
         ../configs/netplan/bb-authns.yaml > $workdir/bb-authns.yaml
-
-    # pushing network config..."
     lxc file push $workdir/bb-authns.yaml authns/etc/netplan/bb-lxc.yaml
     lxc exec authns -- sh -c 'chmod 600 /etc/netplan/bb-lxc.yaml'
     lxc exec authns -- sh -c "echo authns.$DOMAIN >/etc/hostname"
     lxc exec authns -- sh -c "hostname authns.$DOMAIN"
     lxc exec authns -- sh -c "echo 127.0.0.222 authns.$DOMAIN >>/etc/hosts"
     lxc exec authns -- sh -c 'netplan apply'
-    lxc exec authns -- sh -c 'apt install -qy bind9'
 
-    # generating authns config
+    # configure authoritative DNS
+    lxc exec authns -- sh -c 'apt install -qy bind9'
     lxc exec authns -- sh -c 'rm -rf /etc/bind/named.conf*'
     lxc file push ../configs/authns/named.conf.options authns/etc/bind/named.conf.options
     lxc file push ../configs/authns/named.conf.local   authns/etc/bind/named.conf.local
     lxc exec authns -- sh -c 'chown -R bind:bind /etc/bind/*'
     lxc exec authns -- sh -c 'mkdir -p /var/lib/bind/zones'
-
     lxc file push ../configs/authns/db.rpz                 authns/var/lib/bind/zones/
     lxc file push ../configs/authns/db.internal            authns/var/lib/bind/zones/
     lxc file push ../configs/authns/db.evilnsip.internal   authns/var/lib/bind/zones/
     lxc file push ../configs/authns/db.badnsname.internal  authns/var/lib/bind/zones/
-
     lxc exec authns -- sh -c 'chown -R bind:bind /var/lib/bind'
 
     # restart server to apply all config changes 
