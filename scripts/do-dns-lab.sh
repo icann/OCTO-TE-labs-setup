22a8a8e474a44840104f462843142517f2a076c5
Ulrich Wisser <ulrich.wisser@icann.org>
2026-07-20 21:22:48 +0200

    do-dns-lab now uses system config and can do all labs

--- scripts/do-dns-lab.sh
+++ scripts/do-dns-lab.sh
@@ -6,225 +6,236 @@
 #
 set -exou
 
-# Check if two arguments are provided
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <group_number> <domain>"
-  exit 1
-fi
+# Load parameters from "deploy-parameters.cfg" file
+. ./deploy-parameters.cfg
 
-# Validate that the first argument is numeric
-if ! [[ "$1" =~ ^[0-9]+$ ]]; then
-  echo "Error: The first argument must be a numeric group number."
-  exit 1
-fi
+# Parse command line arguments
+# (allowed values: all, 1, 2, ..., $NETWORKS)
 
-# save arguments to named variables
-GRP=$1
-DOMAIN=$2
+arg="${1:-}"
 
-# Let the fun begin
-echo "Fixing group $GRP $DOMAIN"
+case "$arg" in
+  all)
+    start=1
+    end=$NETWORKS
+    ;;
+  ''|*[!0-9]*)
+    echo "Usage: $0 [all|NUMBER]"
+    exit 1
+    ;;
+  *)
+    if (( arg < 1 || arg > NETWORKS )); then
+      echo "Error: number must be between 1 and $NETWORKS"
+      exit 1
+    fi
+    start=$arg
+    end=$arg
+    ;;
+esac
 
-#
-# resolv1 - bind9
-#
-lxc exec grp$GRP-resolv1 -- sh -c 'mv /etc/resolv.conf /etc/resolv.conf.orig'
-lxc exec grp$GRP-resolv1 -- sh -c 'echo "nameserver 9.9.9.9"|sudo tee /etc/resolv.conf'
+for ((GRP=start; GRP<=end; GRP++)); do
 
-cat <<EOF >/tmp/named.conf.options.resolv1
-options {
-  directory "/var/cache/bind";
-  dnssec-validation no;
-  listen-on port 53 { localhost; 100.100.0.0/16; };
-  listen-on-v6 port 53 { localhost; fd89:59e0::/32; };
-  allow-query { any; };
-  recursion yes;
-};
-EOF
+    #
+    # resolv1 - bind9
+    #
+    lxc exec grp$GRP-resolv1 -- sh -c 'mv /etc/resolv.conf /etc/resolv.conf.orig'
+    lxc exec grp$GRP-resolv1 -- sh -c 'echo "nameserver 9.9.9.9"|sudo tee /etc/resolv.conf'
 
-cat <<EOF >/tmp/named.conf.local.resolv1
-zone "internal." {
-    type forward;
-    forwarders { 100.64.0.54; };
-    forward only;  // only forward, no fallback to root hints
-};
-EOF
-
-lxc exec grp$GRP-resolv1 -- sh -c 'apt install -qy bind9'
-lxc file push /tmp/named.conf.options.resolv1 grp$GRP-resolv1/etc/bind/named.conf.options
-lxc file push /tmp/named.conf.local.resolv1   grp$GRP-resolv1/etc/bind/named.conf.local
-lxc exec grp$GRP-resolv1 -- sh -c 'systemctl enable named'
-lxc exec grp$GRP-resolv1 -- sh -c 'systemctl restart named'
-lxc exec grp$GRP-resolv1 -- sh -c 'mv /etc/resolv.conf.orig /etc/resolv.conf'
-
-#
-# resolv2 - unbound
-#
-cat <<EOF > /tmp/unbound.conf
-server:
-        interface: 0.0.0.0
-        interface: ::0
-
-        access-control: 0.0.0.0/0 allow
-        access-control: ::/0 allow
-
-        port: 53
-
-        do-udp: yes
-        do-tcp: yes
-        do-ip4: yes
-        do-ip6: yes
-
-remote-control:
-    control-enable: yes
-
-stub-zone:
-    name: "internal."
-    stub-addr: 100.64.0.54
-EOF
-
-lxc exec grp$GRP-resolv2 -- sh -c 'apt install -qy unbound'
-lxc exec grp$GRP-resolv2 -- sh -c 'rm -rf /etc/unbound/unbound.conf.d'
-lxc file push /tmp/unbound.conf grp$GRP-resolv2/etc/unbound/unbound.conf
-lxc exec grp$GRP-resolv2 -- sh -c 'unbound-control-setup'
-lxc exec grp$GRP-resolv2 -- sh -c 'systemctl enable unbound'
-lxc exec grp$GRP-resolv2 -- sh -c 'systemctl restart unbound'
-
-#
-# soa - bind9
-#
-cat <<EOF > /tmp/db.grp$GRP
-; grp${GRP}
-
-\$TTL    30
-@       IN      SOA     ${DOMAIN}. te-labs.icann.org. (
-                              1         ; Serial
-                         604800         ; Refresh
-                          86400         ; Retry
-                        2419200         ; Expire
-                             30 )       ; Negative Cache TTL
-@           MX          0 .
-@           NS          ${DOMAIN}.
-@           TXT         "DNS IS FUN"
-@           TXT         "v=spf1 -all"
-ns1         A           100.100.${GRP}.130
-ns1         AAAA        fd89:59e0:${GRP}:128::130
-ns2         A           100.100.${GRP}.131
-ns2         AAAA        fd89:59e0:${GRP}:128::131
-EOF
-
-cat <<EOF > /tmp/named.conf.local.primary
-zone "grp${GRP}.${DOMAIN}." {
-	type primary;
-	file "/var/lib/bind/zones/db.grp${GRP}";
-	allow-transfer { any; };
-	also-notify {
-		100.100.${GRP}.130; 
-		100.100.${GRP}.131; 
-		fd89:59e0:${GRP}:128::130; 
-		fd89:59e0:${GRP}:128::131; 
-	};
-}; 
-EOF
-
-cat <<EOF > /tmp/named.conf.options.primary
-options {
+    cat <<EOF >/tmp/named.conf.options.resolv1
+    options {
     directory "/var/cache/bind";
-    server-id "hidden primary";
-    version "grp${GRP}";
-    hostname "grp${GRP}-soa";
     dnssec-validation no;
     listen-on port 53 { localhost; 100.100.0.0/16; };
     listen-on-v6 port 53 { localhost; fd89:59e0::/32; };
     allow-query { any; };
-    allow-transfer { any; };
-    also-notify { any; };
     recursion yes;
-};
-EOF
-
-lxc exec grp$GRP-soa -- sh -c 'apt install -qy bind9'
-lxc exec grp$GRP-soa -- sh -c 'mkdir -p /var/lib/bind/zones'
-lxc file push /tmp/named.conf.options.primary grp$GRP-soa/etc/bind/named.conf.options
-lxc file push /tmp/named.conf.local.primary grp$GRP-soa/etc/bind/named.conf.local
-lxc file push /tmp/db.grp$GRP grp$GRP-soa/var/lib/bind/zones/db.grp$GRP
-lxc exec grp$GRP-soa -- sh -c 'systemctl enable named'
-lxc exec grp$GRP-soa -- sh -c 'systemctl restart named'
-lxc exec grp$GRP-soa -- sh -c 'chown -R bind:bind /var/lib/bind'
-
-#
-# ns1 - bind9 secondary
-#
-cat <<EOF > /tmp/named.conf.local.secondary
-zone "grp${GRP}.${DOMAIN}" {
-    type secondary;
-    file "/var/lib/bind/zones/db.grp${GRP}.secondary";
-    masters { 
-        100.100.${GRP}.66; 
-        fd89:59e0:${GRP}:64::66;
     };
-};
 EOF
 
-cat <<EOF > /tmp/named.conf.options.secondary
-options {
-    directory "/var/cache/bind";
-    server-id "${GRP} Secondary server_id";
-    version "grp${GRP}";
-    hostname "${GRP} Secondary host_name";
-    dnssec-validation no;
-    listen-on port 53 { localhost; 100.100.0.0/16; };
-    listen-on-v6 port 53 { localhost; fd89:59e0::/32; };
-    allow-query { any; };
-    recursion yes;
-    cookie-secret "71ff147d946b942ed66e608b64dc54c9";
-};
+    cat <<EOF >/tmp/named.conf.local.resolv1
+    zone "internal." {
+        type forward;
+        forwarders { 100.64.0.54; };
+        forward only;  // only forward, no fallback to root hints
+    };
 EOF
 
-lxc exec grp$GRP-ns1 -- sh -c 'apt install -qy bind9'
-lxc exec grp$GRP-ns1 -- sh -c 'mkdir -p /var/lib/bind/zones'
-lxc file push /tmp/named.conf.options.secondary grp$GRP-ns1/etc/bind/named.conf.options
-lxc file push /tmp/named.conf.local.secondary grp$GRP-ns1/etc/bind/named.conf.local
-lxc exec grp$GRP-ns1 -- sh -c 'touch /var/lib/bind/zones/db.grp$GRP.secondary'
-lxc exec grp$GRP-ns1 -- sh -c 'systemctl enable named'
-lxc exec grp$GRP-ns1 -- sh -c 'systemctl restart named'
-lxc exec grp$GRP-ns1 -- sh -c 'chown -R bind:bind /var/lib/bind'
+    lxc exec grp$GRP-resolv1 -- sh -c 'apt install -qy bind9'
+    lxc file push /tmp/named.conf.options.resolv1 grp$GRP-resolv1/etc/bind/named.conf.options
+    lxc file push /tmp/named.conf.local.resolv1   grp$GRP-resolv1/etc/bind/named.conf.local
+    lxc exec grp$GRP-resolv1 -- sh -c 'systemctl enable named'
+    lxc exec grp$GRP-resolv1 -- sh -c 'systemctl restart named'
+    lxc exec grp$GRP-resolv1 -- sh -c 'mv /etc/resolv.conf.orig /etc/resolv.conf'
 
-#
-# ns2 - nsd secondary
-#
-cat <<EOF > /tmp/nsd.conf
-server:
-    log-only-syslog: yes
-    zonesdir: "/var/lib/nsd"
-    nsid: "ascii_grp${GRP} NSD nsid"
-    hide-version: no
-    hide-identity: no
-    cookie-secret: "71ff147d946b942ed66e608b64dc54c9"
-    answer-cookie: yes
+    #
+    # resolv2 - unbound
+    #
+    cat <<EOF > /tmp/unbound.conf
+    server:
+            interface: 0.0.0.0
+            interface: ::0
 
-pattern:
-    name: "fromprimary"
-    allow-notify: 100.100.${GRP}.66 NOKEY
-    allow-notify: fd89:59e0:${GRP}:64::66 NOKEY
-    allow-notify: fd89:59e0:${GRP}::2 NOKEY
-    request-xfr: AXFR 100.100.${GRP}.66 NOKEY
-    request-xfr: AXFR fd89:59e0:${GRP}:64::66 NOKEY
-    request-xfr: AXFR fd89:59e0:${GRP}::2 NOKEY
+            access-control: 0.0.0.0/0 allow
+            access-control: ::/0 allow
 
-zone:
-    name: "grp${GRP}.${DOMAIN}."
-    zonefile: "db.grp${GRP}.secondary"
-    include-pattern: "fromprimary"
+            port: 53
+
+            do-udp: yes
+            do-tcp: yes
+            do-ip4: yes
+            do-ip6: yes
+
+    remote-control:
+        control-enable: yes
+
+    stub-zone:
+        name: "internal."
+        stub-addr: 100.64.0.54
 EOF
 
-lxc exec grp$GRP-ns2 -- sh -c 'apt install -qy nsd'
-lxc exec grp$GRP-ns2 -- sh -c 'mkdir -p /var/lib/nsd'
-lxc file push /tmp/nsd.conf grp$GRP-ns2/etc/nsd/nsd.conf
-lxc exec grp$GRP-ns2 -- sh -c 'touch /var/lib/nsd/db.grp$GRP.secondary'
-lxc exec grp$GRP-ns2 -- sh -c 'systemctl enable nsd'
-lxc exec grp$GRP-ns2 -- sh -c 'systemctl restart nsd'
-lxc exec grp$GRP-ns2 -- sh -c 'chown -R nsd:nsd /var/lib/nsd'
+    lxc exec grp$GRP-resolv2 -- sh -c 'apt install -qy unbound'
+    lxc exec grp$GRP-resolv2 -- sh -c 'rm -rf /etc/unbound/unbound.conf.d'
+    lxc file push /tmp/unbound.conf grp$GRP-resolv2/etc/unbound/unbound.conf
+    lxc exec grp$GRP-resolv2 -- sh -c 'unbound-control-setup'
+    lxc exec grp$GRP-resolv2 -- sh -c 'systemctl enable unbound'
+    lxc exec grp$GRP-resolv2 -- sh -c 'systemctl restart unbound'
 
-# Done
-echo "All done for group $GRP $DOMAIN"
+    #
+    # soa - bind9
+    #
+    cat <<EOF > /tmp/db.grp$GRP
+    ; grp${GRP}
+
+    \$TTL    30
+    @       IN      SOA     ${DOMAIN}. te-labs.icann.org. (
+                                1         ; Serial
+                            604800         ; Refresh
+                            86400         ; Retry
+                            2419200         ; Expire
+                                30 )       ; Negative Cache TTL
+    @           MX          0 .
+    @           NS          ${DOMAIN}.
+    @           TXT         "DNS IS FUN"
+    @           TXT         "v=spf1 -all"
+    ns1         A           100.100.${GRP}.130
+    ns1         AAAA        fd89:59e0:${GRP}:128::130
+    ns2         A           100.100.${GRP}.131
+    ns2         AAAA        fd89:59e0:${GRP}:128::131
+EOF
+
+    cat <<EOF > /tmp/named.conf.local.primary
+    zone "grp${GRP}.${DOMAIN}." {
+        type primary;
+        file "/var/lib/bind/zones/db.grp${GRP}";
+        allow-transfer { any; };
+        also-notify {
+            100.100.${GRP}.130; 
+            100.100.${GRP}.131; 
+            fd89:59e0:${GRP}:128::130; 
+            fd89:59e0:${GRP}:128::131; 
+        };
+    }; 
+EOF
+
+    cat <<EOF > /tmp/named.conf.options.primary
+    options {
+        directory "/var/cache/bind";
+        server-id "hidden primary";
+        version "grp${GRP}";
+        hostname "grp${GRP}-soa";
+        dnssec-validation no;
+        listen-on port 53 { localhost; 100.100.0.0/16; };
+        listen-on-v6 port 53 { localhost; fd89:59e0::/32; };
+        allow-query { any; };
+        allow-transfer { any; };
+        also-notify { any; };
+        recursion yes;
+    };
+EOF
+
+    lxc exec grp$GRP-soa -- sh -c 'apt install -qy bind9'
+    lxc exec grp$GRP-soa -- sh -c 'mkdir -p /var/lib/bind/zones'
+    lxc file push /tmp/named.conf.options.primary grp$GRP-soa/etc/bind/named.conf.options
+    lxc file push /tmp/named.conf.local.primary grp$GRP-soa/etc/bind/named.conf.local
+    lxc file push /tmp/db.grp$GRP grp$GRP-soa/var/lib/bind/zones/db.grp$GRP
+    lxc exec grp$GRP-soa -- sh -c 'systemctl enable named'
+    lxc exec grp$GRP-soa -- sh -c 'systemctl restart named'
+    lxc exec grp$GRP-soa -- sh -c 'chown -R bind:bind /var/lib/bind'
+
+    #
+    # ns1 - bind9 secondary
+    #
+    cat <<EOF > /tmp/named.conf.local.secondary
+    zone "grp${GRP}.${DOMAIN}" {
+        type secondary;
+        file "/var/lib/bind/zones/db.grp${GRP}.secondary";
+        masters { 
+            100.100.${GRP}.66; 
+            fd89:59e0:${GRP}:64::66;
+        };
+    };
+EOF
+
+    cat <<EOF > /tmp/named.conf.options.secondary
+    options {
+        directory "/var/cache/bind";
+        server-id "${GRP} Secondary server_id";
+        version "grp${GRP}";
+        hostname "${GRP} Secondary host_name";
+        dnssec-validation no;
+        listen-on port 53 { localhost; 100.100.0.0/16; };
+        listen-on-v6 port 53 { localhost; fd89:59e0::/32; };
+        allow-query { any; };
+        recursion yes;
+        cookie-secret "71ff147d946b942ed66e608b64dc54c9";
+    };
+EOF
+
+    lxc exec grp$GRP-ns1 -- sh -c 'apt install -qy bind9'
+    lxc exec grp$GRP-ns1 -- sh -c 'mkdir -p /var/lib/bind/zones'
+    lxc file push /tmp/named.conf.options.secondary grp$GRP-ns1/etc/bind/named.conf.options
+    lxc file push /tmp/named.conf.local.secondary grp$GRP-ns1/etc/bind/named.conf.local
+    lxc exec grp$GRP-ns1 -- sh -c 'touch /var/lib/bind/zones/db.grp$GRP.secondary'
+    lxc exec grp$GRP-ns1 -- sh -c 'systemctl enable named'
+    lxc exec grp$GRP-ns1 -- sh -c 'systemctl restart named'
+    lxc exec grp$GRP-ns1 -- sh -c 'chown -R bind:bind /var/lib/bind'
+
+    #
+    # ns2 - nsd secondary
+    #
+    cat <<EOF > /tmp/nsd.conf
+    server:
+        log-only-syslog: yes
+        zonesdir: "/var/lib/nsd"
+        nsid: "ascii_grp${GRP} NSD nsid"
+        hide-version: no
+        hide-identity: no
+        cookie-secret: "71ff147d946b942ed66e608b64dc54c9"
+        answer-cookie: yes
+
+    pattern:
+        name: "fromprimary"
+        allow-notify: 100.100.${GRP}.66 NOKEY
+        allow-notify: fd89:59e0:${GRP}:64::66 NOKEY
+        allow-notify: fd89:59e0:${GRP}::2 NOKEY
+        request-xfr: AXFR 100.100.${GRP}.66 NOKEY
+        request-xfr: AXFR fd89:59e0:${GRP}:64::66 NOKEY
+        request-xfr: AXFR fd89:59e0:${GRP}::2 NOKEY
+
+    zone:
+        name: "grp${GRP}.${DOMAIN}."
+        zonefile: "db.grp${GRP}.secondary"
+        include-pattern: "fromprimary"
+EOF
+
+    lxc exec grp$GRP-ns2 -- sh -c 'apt install -qy nsd'
+    lxc exec grp$GRP-ns2 -- sh -c 'mkdir -p /var/lib/nsd'
+    lxc file push /tmp/nsd.conf grp$GRP-ns2/etc/nsd/nsd.conf
+    lxc exec grp$GRP-ns2 -- sh -c 'touch /var/lib/nsd/db.grp$GRP.secondary'
+    lxc exec grp$GRP-ns2 -- sh -c 'systemctl enable nsd'
+    lxc exec grp$GRP-ns2 -- sh -c 'systemctl restart nsd'
+    lxc exec grp$GRP-ns2 -- sh -c 'chown -R nsd:nsd /var/lib/nsd'
+
+    # Done
+    echo "All done for group $GRP"
+done
